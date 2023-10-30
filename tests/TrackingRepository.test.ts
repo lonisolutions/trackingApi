137141ab1f4b08fd2ef529f46d92de68845715f9
lonisolutions <afshin.loni@gmail.com>
2023-10-30 12:28:03 +0100

    add unittests for repositories

---
+++ tests/TrackingRepository.test.ts
@@ -0,0 +1,46 @@
+import TrackingRepository from "../repository/TrackingRepository";
+import knexInstance from "../db";
+
+jest.mock("../db");
+
+describe("TrackingRepository", () => {
+  let repo: TrackingRepository;
+
+  beforeEach(() => {
+    (knexInstance as jest.Mocked<any>).mockClear();
+
+    repo = new TrackingRepository();
+  });
+
+  describe("findByTrackingNumberAndCarrier", () => {
+    it("should fetch shipments by tracking number and carrier", async () => {
+      const mockShipments = [
+        {
+          tracking_number: "TN123",
+          carrier: "DHL",
+        },
+      ];
+
+      (knexInstance as jest.Mocked<any>).mockReturnValue({
+        where: jest.fn().mockReturnThis(),
+        andWhere: jest.fn().mockResolvedValue(mockShipments),
+      } as any);
+
+      const result = await repo.findByTrackingNumberAndCarrier("TN123", "DHL");
+      expect(result).toEqual(mockShipments);
+    });
+
+    it("should return an empty array when no shipments found", async () => {
+      (knexInstance as jest.Mocked<any>).mockReturnValue({
+        where: jest.fn().mockReturnThis(),
+        andWhere: jest.fn().mockResolvedValue([]),
+      } as any);
+
+      const result = await repo.findByTrackingNumberAndCarrier(
+        "TN123",
+        "wrong carrier"
+      );
+      expect(result).toEqual([]);
+    });
+  });
+});
---
+++ tests/WeatherRepository.test.ts
@@ -0,0 +1,65 @@
+// WeatherRepository.test.ts
+
+import WeatherRepository from "../repository/WeatherRepository";
+import knexInstance from "../db";
+import { WeatherData } from "types";
+
+jest.mock("../db");
+
+describe("WeatherRepository", () => {
+  let repo: WeatherRepository;
+
+  beforeEach(() => {
+    (knexInstance as jest.Mocked<any>).mockClear();
+
+    repo = new WeatherRepository();
+  });
+
+  describe("getWeatherByPostalCode", () => {
+    it("should fetch weather data by postal code", async () => {
+      const mockData = { postal_code: "12345", temperature: 25 };
+
+      (knexInstance as jest.Mocked<any>).mockReturnValue({
+        where: jest.fn().mockReturnValue({
+          first: jest.fn().mockResolvedValue(mockData),
+        }),
+      } as any);
+
+      const result = await repo.getWeatherByPostalCode("12345");
+      expect(result).toEqual(mockData);
+    });
+
+    it("should return undefined when no data is found", async () => {
+      (knexInstance as jest.Mocked<any>).mockReturnValue({
+        where: jest.fn().mockReturnValue({
+          first: jest.fn().mockResolvedValue(undefined),
+        }),
+      } as any);
+
+      const result = await repo.getWeatherByPostalCode("67890");
+      expect(result).toBeUndefined();
+    });
+  });
+
+  describe("saveWeather", () => {
+    it("should save or update weather data", async () => {
+      const mockData = {
+        postal_code: "12345",
+        temperature: 25,
+      } as WeatherData;
+
+      (knexInstance as jest.Mocked<any>).mockReturnValue({
+        insert: jest.fn().mockReturnValue({
+          onConflict: jest.fn().mockReturnValue({
+            merge: jest.fn().mockResolvedValue(undefined),
+          }),
+        }),
+      } as any);
+
+      await repo.saveWeather(mockData);
+
+      expect(knexInstance).toHaveBeenCalledWith("weather");
+      expect(knexInstance().insert).toHaveBeenCalledWith(mockData);
+    });
+  });
+});
